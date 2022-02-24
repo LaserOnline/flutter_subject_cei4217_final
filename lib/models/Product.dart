a992f36a86a5baaeca9a5fa191c57f18fd4bcb80
laseronline <razeronline@hotmail.com>
2022-02-24 23:47:39 +0700

    update

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -37,4 +37,46 @@ List<Product> demo_product = [
     price: 85,
     bgColor: const Color(0xFFEEEEED),
   ),
+  Product(
+    image: "assets/images/fas05.png",
+    title: "เสาชิงช้า",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas06.png",
+    title: "ตลาดน้ำตลิ่งชัน",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas07.png",
+    title: "สวนรถไฟ จตุจักร",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas08.png",
+    title: "ท่ามหาราช",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas09.png",
+    title: "ภูเขาทอง วัดสระเกศ",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas10.png",
+    title: "เยาวราช",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+  Product(
+    image: "assets/images/fas11.png",
+    title: "สนามหลวง",
+    price: 85,
+    bgColor: const Color(0xFFEEEEED),
+  ),
 ];
